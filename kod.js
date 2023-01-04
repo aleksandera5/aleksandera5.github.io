323a87aa45e614d5fa3e01b0d2bbc66297854bec
aleksandera5 <77803441+aleksandera5@users.noreply.github.com>
2023-01-04 19:26:20 +0100

    Add files via upload

--- kod.js
+++ kod.js
@@ -1,52 +1,26 @@
-
 var cena = 0;
-var licz = 0;
-var licz2 = 0;
-var licz3 = 0;
-function zamow1() {
-    licz = licz+1;
-    cena = cena + 30;
-    document.getElementById("dozaplacenia").innerHTML= cena + "zł";
-    document.getElementById("ussushi").innerHTML="Usuń z koszyka maki sushi";
+
+function zamow14() {
+    cena = cena + 14;
+    document.getElementById("dozaplacenia").innerHTML=" " + cena + "zł";
 }
 
-function zamow2() {
-    licz2 = licz2 + 1;
-    cena = cena + 25;
-    document.getElementById("dozaplacenia").innerHTML= cena + "zł";
-    document.getElementById("usonigiri").innerHTML="Usuń z koszyka onigiri";
+function zamow22() {
+    cena = cena + 22;
+    document.getElementById("dozaplacenia").innerHTML=" " + cena + "zł";
 }
 
-function zamow3() {
-    licz3 = licz3 + 1;
-    cena = cena + 20;
-    document.getElementById("dozaplacenia").innerHTML= cena + "zł";
-    document.getElementById("usgyoza").innerHTML="Usuń z koszyka pierożki gyoza";
+function zamow12() {
+    cena = cena + 12;
+    document.getElementById("dozaplacenia").innerHTML=" " + cena + "zł";
 }
 
-function usunsushi () {
-    cena= cena - 30;
-    document.getElementById("dozaplacenia").innerHTML= cena + "zł";
-   licz = licz - 1;
-   if (licz==0){
-        document.getElementById("ussushi").innerHTML= "";
-    }
+function zamow19() {
+    cena = cena + 19;
+    document.getElementById("dozaplacenia").innerHTML=" " + cena + "zł";
 }
 
-function usunonigiri () {
-    cena = cena - 25;
-    document.getElementById("dozaplacenia").innerHTML= cena + "zł";
-    licz2 = licz2 - 1;
-    if (licz2==0){
-         document.getElementById("usonigiri").innerHTML= "";
-     }
-}
-
-function usungyoza() {
-    cena = cena - 20;
-    document.getElementById("dozaplacenia").innerHTML= cena + "zł";
-    licz3 = licz3 - 1;
-    if (licz3==0){
-         document.getElementById("usgyoza").innerHTML= "";
-     }
+function usun() {
+    cena = 0;
+    document.getElementById("dozaplacenia").innerHTML= " ";
 }
